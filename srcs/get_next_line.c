2baa4fcfdfc38ad1c1533d7a0783184114cd891d
Justin Lee <triplez@justinlee.sg>
2024-06-10 17:46:09 +0800

    Refactor get_next_line.c and get_next_line_utils.c for improved readability and performance

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 14:37:16 by welee             #+#    #+#             */
-/*   Updated: 2024/06/07 16:13:30 by welee            ###   ########.fr       */
+/*   Updated: 2024/06/10 17:40:23 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,12 +46,9 @@ static char	*read_to_newline(int fd, char *stored)
 		buffer[bytes_read] = '\0';
 		temp = stored;
 		stored = ft_strjoin(temp, buffer);
-		if (!stored)
-		{
-			free(temp);
-			return (NULL);
-		}
 		free(temp);
+		if (!stored)
+			return (NULL);
 	}
 	return (stored);
 }
